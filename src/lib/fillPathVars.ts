d3eb4ba503043ab5a6f5c322aaef82bee790c347
Carlos Joo <29982416@qq.com>
2022-07-02 15:46:57 +0800

    update

---
+++ src/lib/fillPathVars.ts
@@ -0,0 +1,26 @@
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import AxiosPlus, { PluginOptions } from "../core";
+import { noop } from "./utils";
+
+let loadingCount = 0;
+export default function polling(
+	axios: AxiosPlus,
+	option: PluginOptions<{
+		prefix: string,
+		suffix: string
+	}>
+) {
+	const regexp = new RegExp(`\\${option.prefix}([^${option.prefix}])+\\${option.suffix}`, 'g')
+	axios.interceptors.request.use(
+		function (config) {
+			console.log("fillpathvars");
+			config.url = config.url.replace(regexp, (_, name) => {
+				let payload = config.params[name] ? config.params : config.data
+				let value = payload[name]
+				delete payload[name]
+				return value
+			})
+			return config;
+		}
+	);
+}
