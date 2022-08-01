a5b88a712413e9ac3aa92efab0b7abe18e35ec61
Carlos Joo <29982416@qq.com>
2022-08-01 17:42:14 +0800

    update

--- src/lib/useHttp.tsx
+++ src/lib/useHttp.tsx
@@ -21,30 +21,31 @@ function createAborter() {
 
 
 export default function useHttp<T extends Callback>(executor: T, option?: Option<T>, deps?: any[]) {
-    const $option: Option<T> & { executor: T } = useMemo(() => {
-        return {
-            immediate: true,
-            ...option,
-            executor
-        }
-    }, [executor, option])
+    const $option: Option<T> & { executor: T } = useMemo(() => ({
+        immediate: true,
+        ...option,
+        executor
+    }), [executor, option])
     const [loading, setLoading] = useState(false)
     const [data, setData] = useState(option?.defaultValue)
     const [error, setError] = useState(null)
+    const [empty, setEmpty] = useState(false)
 
     const handle: CancellablePromise = useCallback(() => {
         let aborter = createAborter()
         setLoading(true)
         const promise: CancellablePromise = Promise.race([$option.executor($option.payload), aborter])
             .then(([data, err]) => {
-                if (err === REASON) return
-                setError(undefined)
+                if (err === REASON) return setEmpty(true)
                 setData(data)
+                setError(null)
+                setEmpty(typeof data === 'object' ? Object.keys(data).length === 0 : false)
                 return res
             })
             .catch(err => {
                 setData($option.defaultValue)
                 setError(err)
+                setEmpty(true)
                 return Promise.reject(err)
             })
             .finally(() => {
@@ -66,6 +67,7 @@ export default function useHttp<T extends Callback>(executor: T, option?: Option
         loading,
         data,
         error,
-        handle
+        handle,
+        empty
     }
 }
